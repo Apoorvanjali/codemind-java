034d2bbb7a1bb97c69a3dc0a0d35413b4a1794b0
Apoorvanjali <119914856+Apoorvanjali@users.noreply.github.com>
2024-01-08 23:07:00 +0530

    import java.util.Scanner; public class Main{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int avg = sc.nextInt();        int b = sc.nextInt();        int c = sc.nextInt();        int a;        a=(3*avg)-b-c;         System.out.println(a);     } }

---
+++ Average_weight.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int avg = sc.nextInt();
+       int b = sc.nextInt();
+       int c = sc.nextInt();
+       int a;
+       a=(3*avg)-b-c;
+        System.out.println(a);
+    }
+}
